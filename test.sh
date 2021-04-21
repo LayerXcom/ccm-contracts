a4a8bae2b1963f9e448b14878006a388043b55dd
jkcomment <jkcomment@gmail.com>
2021-04-21 20:03:09 +0900

    add environment variables

--- test.sh
+++ test.sh
@@ -9,9 +9,19 @@ solc -o contract-build --bin --abi --optimize --overwrite \
     contracts/AnonifyWithEnclaveKey.sol \
     contracts/Factory.sol
 
-export CONFIRMATIONS=0
-export ETH_URL=http://172.16.11.2:8545
+# export ETH_URL=http://172.16.11.2:8545
 export PJ_NAME=anonify-contracts
+# Besu用
+export CONFIRMATIONS=1
+export ETH_URL=http://besur64ps.eastus.cloudapp.azure.com/jsonrpc
+export SIGNER_PRI_KEY=eeb069697a386bb8d5a985af0a4a227be39e9eee8d8d73deadb74672b9605ad3
+export CHAIN_ID=2018
+
+# ganache用
+# export CONFIRMATIONS=0
+# export ETH_URL=http://localhost:8545
+# export CHAIN_ID=1337
+# export SIGNER_PRI_KEY=eeb069697a386bb8d5a985af0a4a227be39e9eee8d8d73deadb74672b9605ad3
 
 PJ_ROOT_DIR=$(cd $(dirname $0) && pwd)
 cd "${PJ_ROOT_DIR}/deployer"
